2fb69908555607e30bb0454bf9fb560d9dcce94d
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-03-15 14:58:57 +0900

    bumped 1.3.10

--- push_docker.sh
+++ push_docker.sh
@@ -5,5 +5,5 @@ set -e
 docker login --username $DOCKER_USER_NAME --password $DOCKER_PASSWORD
 
 # docker push kwmt/flutter:base
-docker push kwmt/flutter:1.3.9
+docker push kwmt/flutter:1.3.10
 docker push kwmt/flutter:latest
